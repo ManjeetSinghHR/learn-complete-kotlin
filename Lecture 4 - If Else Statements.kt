f43abacec22fdecf97a91b6a46fc1527f7e57105
Manjeet Singh <ManjeetSingh26071999@Gmail.com>
2023-10-05 16:00:43 +0530

    If Else Statements

--- Lecture 4 - If Else Statements.kt
+++ Lecture 4 - If Else Statements.kt
@@ -29,5 +29,26 @@ fun main() {
         val result1 = if (NUM > 5)  "10 Is Greater Than 5" else if (NUM < 5) "10 Is Less Than 5" else "10 Is Equal To 5"
     
         println("Result Is - $result1")
-    
-    }
+
+        
+// Ques 2. Let, Suppose we have 5 users in our app, we have to check how many users have our Premium Subscriptions.
+// Solution - 1. We have to create 5 users in DB With Unique ID -
+
+    val user1 = "Normal User"
+    val user2 = "Premium"
+    val user3 = "Normal User"
+    val user4 = "Premium"
+    val user5 = "Premium"
+
+    var totalSub = 0
+    // 2. Here we store total Premium Subscriptions
+
+    if (user1 == "Premium") totalSub = totalSub + 1
+    if (user2 == "Premium") totalSub = totalSub + 1
+    if (user3 == "Premium") totalSub = totalSub + 1
+    if (user4 == "Premium") totalSub = totalSub + 1
+    if (user5 == "Premium") totalSub = totalSub + 1
+
+    println("Total Premium Subscriptions Are - $totalSub")
+
+}
