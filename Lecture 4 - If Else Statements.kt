a863a25a6d779332dc67bad1014697147eba8fd1
Manjeet Singh <ManjeetSingh26071999@Gmail.com>
2023-10-05 12:25:42 +0530

    If Else Statements

--- Lecture 4 - If Else Statements.kt
+++ Lecture 4 - If Else Statements.kt
@@ -1,5 +1,33 @@
-fun main(args: Array<String>) {
+fun main() {
 
+    // Ques 1. Find, Is 10 (Greater, Less Or Equal) To 5.
     
+        val NUM = 10
     
-}
+        if (NUM > 5) {
+            println("10 Is Greater Than 5")
+        } else if (NUM < 5) {
+            println("10 Is Less Than 5")
+        } else {
+            println("10 Is Equal To 5")
+        }
+    
+    // We can also write if-else statements as an expression, Here is an example -
+    
+        val result = if (NUM > 5) {
+            ("10 Is Greater Than 5")
+        } else if (NUM < 5) {
+            ("10 Is Less Than 5")
+        } else {
+            ("10 Is Equal To 5")
+        }
+    
+        println("Result Is - $result")
+    
+    // We can also write this code in a single line -
+    
+        val result1 = if (NUM > 5)  "10 Is Greater Than 5" else if (NUM < 5) "10 Is Less Than 5" else "10 Is Equal To 5"
+    
+        println("Result Is - $result1")
+    
+    }
