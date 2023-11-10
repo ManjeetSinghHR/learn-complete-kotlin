3a001685d005c9f47213d70ac4e5e97ccdee1672
Manjeet Singh <ManjeetSingh26071999@Gmail.com>
2023-11-10 12:58:15 +0530

    Kotlin First Program

--- Lecture 1 - Kotlin First Program With Syntax.kt
+++ Lecture 1 - Kotlin First Program With Syntax.kt
@@ -2,6 +2,6 @@ fun main() {
     
     println("My Name Is Manjeet Singh")
     println(5+5) // println Is Used To Print In A Line
-    print(2+2)   // print used to print normally without line1
+    print(2+2)   // print used to print normally without line
     
 }
