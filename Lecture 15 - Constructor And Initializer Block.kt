55839c4ab7c5ce2a7d536bc577d295fdc2223e81
Manjeet Singh <ManjeetSingh26071999@Gmail.com>
2023-11-16 20:12:29 +0530

    Constructor And Initializer Block

--- Lecture 15 - Constructor And Initializer Block.kt
+++ Lecture 15 - Constructor And Initializer Block.kt
@@ -7,7 +7,10 @@ fun main() {
     val person2 = Person2("MS",22)
     println(person2.pAge)
     println(person2.pName)
-    val person3 = Automobile(carName = "Audi", carEngineType = "Petrol")
+    val person3 = Automobile(carName = "Audi", carEngineType = "Petrol") // values for secondary constructor
+    val person4 = Automobile("Swift",4, 5, "Diesel") // values for primary constructor
+    println(person3.name)
+    println(person4.name)
 }
 class Person1() // There is no constructor
 {
@@ -27,5 +30,7 @@ class Automobile(val name:String, val tyres:Int, val maxSeating:Int, var engineT
     init {
         println("This is 2nd initializer block")
     }
-    constructor(carName:String, carEngineType:String):this(carName,tyres = 4, maxSeating = 4, carEngineType)
+    constructor(carName:String, carEngineType:String) // This is secondary constructor
+    :this(carName,tyres = 4, maxSeating = 5, carEngineType) // This is the method to call the primary constructor
+    // It is compulsory to call the primary constructor after the secondary constructor
 }
