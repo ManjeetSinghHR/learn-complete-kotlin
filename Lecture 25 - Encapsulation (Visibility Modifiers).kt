1ed5929a91071019803dfb1a65b329188c28868e
Manjeet Singh <ManjeetSingh26071999@Gmail.com>
2023-11-23 21:34:38 +0530

    Encapsulation (Visibility Modifiers)

---
+++ Lecture 25 - Encapsulation (Visibility Modifiers).kt
@@ -0,0 +1,22 @@
+fun main() {
+    val obA = A()
+    val obB = B()
+    obA.p
+    obB.p
+}
+open class A
+{
+    public var p = 10
+    private var q = 10
+    internal var r = 10
+    protected var s = 10
+}
+class B:A()
+{
+    fun test(){
+        println(p)
+        // println(q)
+        println(r)
+        println(s)
+    }
+}
