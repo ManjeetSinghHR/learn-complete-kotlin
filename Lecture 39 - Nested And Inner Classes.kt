a5d9644e1dce624de94662141c7c32f391e96b98
Manjeet Singh <ManjeetSingh26071999@Gmail.com>
2023-11-27 13:42:00 +0530

    Nested And Inner Classes

--- Lecture 39 - Nested And Inner Classes.kt
+++ Lecture 39 - Nested And Inner Classes.kt
@@ -1,27 +1,28 @@
 fun main() {
     // Making a class inside a class known as "Nested" class.
-    val a = Normal()
-    a.a
-    val b = Normal.Nested()
-    b.test()
-    val c = Normal1().Nested()
-    c.test()
+    val a = Normal() // this is the object of Normal class
+    a.a // Property
+    val b = Normal.Nested() // object of Nested class
+    b.test() // Function
+    val c = Normal1().Nested() // Object of "Normal1" & "Nested" Class
+    c.test() // Function
 }
 class Normal
 {
     var a = 0
-    class Nested{
+    class Nested{ // It's a nested class with no existence and this only work with "Normal Class"
         fun test(){
-            println("I am in Nested Class")
+            println("I am in Nested Class") // we cannot access property of Normal class directly in Nested class. E.g. - "$a"
         }
     }
 }
 class Normal1
 {
     var a = 0
-    inner class Nested{
+    inner class Nested{ // We use "inner" keyword to access the property of "Normal1 Class"
         fun test(){
-            println("I am in Nested Class $a")
+            println("I am in Nested Class $a") // "$a" property of "Normal1 Class"
         }
     }
 }
+// ---------> We generally use "Nested Class" in "Android Recycler View" When we define their "adapter" <---------
