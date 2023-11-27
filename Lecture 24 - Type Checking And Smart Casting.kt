666d57312ebbec27600367eaf7c6331d493dd364
Manjeet Singh <ManjeetSingh26071999@Gmail.com>
2023-11-27 17:24:11 +0530

    Type Checking And Smart Casting

--- Lecture 24 - Type Checking And Smart Casting.kt
+++ Lecture 24 - Type Checking And Smart Casting.kt
@@ -1,7 +1,18 @@
 fun main() {
+    // "is" - It's a type checking operator.
+    // We use "as" to smart-cast the properties
+    // It's help to select properties in runtime
+    // Help to reduce errors
     callNew()
     val c:Array<OR> = arrayOf(NewPlayer("Singh"), NewCircle(5.0), NewCircle(6.0), NewCircle(7.0))
-    for (i in c) if (i is NewCircle) println(i.area()) else (i as NewPlayer).sayMyName()
+    for (i in c){
+        if (i is NewCircle){ // "is" - type checking operator
+            println(i.area())
+        }
+        else{
+            (i as NewPlayer).sayMyName() // "as" - to Smart-Cast the properties
+        }
+    }
 }
 fun callNew() {
     val a = NewCircle(2.0)
@@ -9,13 +20,21 @@ fun callNew() {
     println(a.area())
     b.drag()
 }
-interface OR { fun drag() }
-abstract class Calculate:OR { abstract fun area():Double }
-class NewCircle(val radius:Double):Calculate() {
+interface OR
+{
+    fun drag()
+}
+abstract class Calculate:OR
+{
+    abstract fun area():Double
+}
+class NewCircle(val radius:Double):Calculate()
+{
     override fun drag() = println("Circle is dragging")
     override fun area(): Double = Math.PI * radius * radius
 }
-class NewPlayer(val name:String):OR{
+class NewPlayer(val name:String):OR
+{
     override fun drag() = println("${name.uppercase()} Is Dragging")
     fun sayMyName() = println("Hey my name is - ${name.uppercase()}")
 }
