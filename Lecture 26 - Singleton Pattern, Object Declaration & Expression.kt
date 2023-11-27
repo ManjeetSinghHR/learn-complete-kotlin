6fa2413d42f75ee3ede05ece302ded18d0f491da
Manjeet Singh <ManjeetSingh26071999@Gmail.com>
2023-11-27 18:50:24 +0530

    Singleton Pattern, Object Declaration & Expression

--- Lecture 26 - Singleton Pattern, Object Declaration & Expression.kt
+++ Lecture 26 - Singleton Pattern, Object Declaration & Expression.kt
@@ -1,23 +1,48 @@
 fun main() {
-    val a = object{
-        fun x() { println("I am object Expression") }
+    // Like classes, we can create objects directly.
+    // Can also create functions in object.
+    // <----- Some Key Points ----->
+    // 1. Cannot create multiple objects with the same name.
+    // 2. No Constructors are allowed.
+    // 3. "Init" block is allowed.
+    // 4. We can inherit classes and interface.
+    // 5. They Follow "Singleton Pattern".
+    // <----------------------------------------------- ----------------------------------------------->
+    // More about "Singleton Pattern" -
+    // It means a class can create the object only at once and, it's multiple objects can't be created known as "Singleton Pattern"
+    val a = object{ // It's an anonymous object. It's replacement of java's anonymous "Inner" class
+        fun x() { // We can also define functions in anonymous object.
+            println("I am object Expression")
+        }
     }
-    a.x()
+    a.x() // calling function from anonymous object
+    // <----------------------------------------------- ----------------------------------------------->
+    // We can also use interfaces directly in our anonymous object
     val b = object:X{
         override fun name() {
             super.name()
             println("Manjeet Singh")
         }
     }
-    b.name()
+    b.name() // calling function
     callX()
     increaseSocialLikes()
 }
-interface X { fun name() = println("I am in X Interface") }
-object A1
+// ............................................... Interface X ...............................................
+interface X
 {
-    fun test() {println("I AM OBJECT A")}
+    fun name() {
+        println("I am in X Interface")
+    }
 }
+// ............................................... Object A1 ...............................................
+object A1 // Simple object Syntax
+{
+    fun test() { // Function in object
+        println("I AM OBJECT A")
+    }
+}
+// ............................................... Function CallX ...............................................
 fun callX() {
     val x = object:X1("Manjeet Singh"){
         override fun fullName() {
@@ -26,29 +51,41 @@ fun callX() {
         }
     }
     x.fullName()
-    val b = A1
-    b.test()
+    val b = A1 // Calling object
+    b.test() // access function of object
 }
+// ............................................... Open Class X1 ...............................................
 open class X1(val name: String)
 {
     open fun fullName() {
         println("Full Name - $name")
     }
 }
-fun increaseSocialLikes() {
+// ............................................... Function Increase Social Likes ...............................................
+fun increaseSocialLikes()
+{
     SocialLikes.increaseFBLikes()
     SocialLikes.increaseFBLikes()
     SocialLikes.increaseInstagramLikes()
     SocialLikes.increaseTwitterLikes()
     SocialLikes.displayLikes()
 }
+// ............................................... Object Social Likes ...............................................
 object SocialLikes
 {
     private var fbLikes: Int = 0
     private var instagram: Int = 0
     private var twitter: Int = 0
-    fun increaseFBLikes() { fbLikes++ }
-    fun increaseInstagramLikes() { instagram++ }
-    fun increaseTwitterLikes() { twitter++ }
-    fun displayLikes() { println("FB Likes - $fbLikes, Instagram Likes - $instagram, Twitter Likes - $twitter") }
+    fun increaseFBLikes() {
+        fbLikes++
+    }
+    fun increaseInstagramLikes() {
+        instagram++
+    }
+    fun increaseTwitterLikes() {
+        twitter++
+    }
+    fun displayLikes() {
+        println("FB Likes - $fbLikes, Instagram Likes - $instagram, Twitter Likes - $twitter")
+    }
 }
