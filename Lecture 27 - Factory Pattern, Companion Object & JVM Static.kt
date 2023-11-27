50cc9afc074288c9d5d32c23ef2901ed5e27ce3f
Manjeet Singh <ManjeetSingh26071999@Gmail.com>
2023-11-27 19:32:43 +0530

    Factory Pattern, Companion Object & JVM Static

--- Lecture 27 - Factory Pattern, Companion Object & JVM Static.kt
+++ Lecture 27 - Factory Pattern, Companion Object & JVM Static.kt
@@ -1,10 +1,31 @@
 fun main() {
-    NewClass.X.display()
-    NewClass1.display()
+    // We can also create object in classes. All the rules will remain same.
+    NewClass.X.display() // Object name "X" is necessary because object is not companion.
+    NewClass1.display() // We can call function directly, because object is companion.
     makePizza()
 }
-class NewClass {object X {fun display() {println("I am object X")}}}
-class NewClass1 {companion object X{fun display() { println("I am Companion Object")}}}
+// ----------------------------->
+class NewClass // Simple class
+{
+    object X { // object inside the class
+        fun display() {
+            println("I am object X")
+        }
+    }
+}
+// -----------------------------> Companion Object -
+// With the help of "companion", we can directly access the object properties and functions, without writing Object name.
+// In a class, there may be only one companion object.
+class NewClass1 // Simple class
+{
+    companion object X { // It's a "companion" object
+        @JvmStatic // ----> Now we can call this object in Java.
+        fun display() {
+            println("I am Companion Object")
+        }
+    }
+}
+// ----------------------------->
 fun makePizza()
 {
     val orderPizza = Pizza.createPizza("Tomato")
@@ -12,9 +33,13 @@ fun makePizza()
     val orderPizza1 = Pizza.createPizza("Capsicum")
     println(orderPizza1)
 }
+// -----------------------------> Factory Pattern <-----------------------------
+// In simple words, factory pattern is a method, we pass parameters, and it returns the object.
+// It is also known as factory of objects.
+// -----> Example of factory pattern using companion <-----
 class Pizza private constructor(private val type: String, private val topping: String)
 {
-    companion object Factory {
+    companion object Factory { // This is our factory object
         fun createPizza(pizzaType: String): Pizza {
             return when (pizzaType) {
                 "Tomato" -> Pizza("Tomato", "Tomato, Cheese")
@@ -27,3 +52,8 @@ class Pizza private constructor(private val type: String, private val topping: S
         return "Pizza Type - $type, Toppings - $topping"
     }
 }
+// -------------------------------------------------- JVM Static --------------------------------------------------
+// Kotlin & Java both are inter-Operable. It means we can call java code in kotlin and kotlin code in java.
+// But, in some cases if the kotlin function is not static, then we cannot call it in java.
+// To make the function Static, we use JVM Static. It helps us to access the code in java.
+// Syntax --> @JvmStatic
